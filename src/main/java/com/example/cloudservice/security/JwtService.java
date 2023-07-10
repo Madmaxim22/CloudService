9f5176d7c43aa84ef48fba4b273761af6cd97876
Maksim Muhomedyarov <madmaxim22@gmail.com>
2023-07-10 14:26:07 +0300

    feat: create generateToken method

--- src/main/java/com/example/cloudservice/security/JwtService.java
+++ src/main/java/com/example/cloudservice/security/JwtService.java
@@ -2,11 +2,17 @@ package com.example.cloudservice.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
 
 @Service
@@ -22,6 +28,28 @@ public class JwtService {
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(
+            Map<String, Objects> extractClaims,
+            UserDetails userDetails
+    ) {
+        return Jwts
+                .builder()
+                .setClaims(extractClaims)
+                // username -> email
+                .setSubject(userDetails.getUsername())
+                // дата создания токена
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                // дата истечения срока токена
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSingKey(), SignatureAlgorithm.HS256)
+                .compact();
+
     }
 
     // получение всех параметров из токена
