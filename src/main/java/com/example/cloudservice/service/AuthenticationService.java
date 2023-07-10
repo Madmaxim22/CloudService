58bbb0dee30cf1f5ee14ef9b060f1b3f7c0a6bb6
Maksim Muhomedyarov <madmaxim22@gmail.com>
2023-07-10 15:51:24 +0300

    refactor: implements registration users

---
+++ src/main/java/com/example/cloudservice/service/AuthenticationService.java
@@ -0,0 +1,40 @@
+package com.example.cloudservice.service;
+
+import com.example.cloudservice.controller.AuthenticationRequest;
+import com.example.cloudservice.controller.AuthenticationResponse;
+import com.example.cloudservice.controller.RegisterRequest;
+import com.example.cloudservice.model.Role;
+import com.example.cloudservice.model.User;
+import com.example.cloudservice.repository.UserRepository;
+import com.example.cloudservice.security.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder encoder;
+    private final JwtService jwtService;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getEmail())
+                .email(request.getEmail())
+                .password(encoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        return null;
+    }
+}
