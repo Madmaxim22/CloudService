150f7b71bd9429f8677afb2dbbfc154f29ab62e2
Maksim Muhomedyarov <madmaxim22@gmail.com>
2023-07-10 14:16:33 +0300

    feat: universal method of parameter extraction

--- src/main/java/com/example/cloudservice/security/JwtService.java
+++ src/main/java/com/example/cloudservice/security/JwtService.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -15,6 +16,12 @@ public class JwtService {
     // метод извлечения логина
     public String extractUsername(String token) {
         return null;
+    }
+
+    // универсальный метод извлечения параметра
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
     }
 
     // получение всех параметров из токена
