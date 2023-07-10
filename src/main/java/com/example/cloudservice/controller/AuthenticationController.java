bafdfc646cbfeae1d90ae08f7ac4c269db29c01c
Maksim Muhomedyarov <madmaxim22@gmail.com>
2023-07-10 15:39:21 +0300

    feat: create AuthenticationResponse

--- src/main/java/com/example/cloudservice/controller/AuthenticationController.java
+++ src/main/java/com/example/cloudservice/controller/AuthenticationController.java
@@ -3,7 +3,6 @@ package com.example.cloudservice.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
