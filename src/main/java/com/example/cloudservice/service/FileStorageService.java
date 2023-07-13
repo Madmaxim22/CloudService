eac1311dbf2e9cab4a446a22e26c854c06ffcef2
Maksim Muhomedyarov <madmaxim22@gmail.com>
2023-07-13 22:09:54 +0300

    feat: create getUser method

--- src/main/java/com/example/cloudservice/service/FileStorageService.java
+++ src/main/java/com/example/cloudservice/service/FileStorageService.java
@@ -1,8 +1,11 @@
 package com.example.cloudservice.service;
 
+import com.example.cloudservice.model.authentication.User;
 import com.example.cloudservice.repository.FileDBRepository;
 import com.example.cloudservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,4 +13,11 @@ import org.springframework.stereotype.Service;
 public class FileStorageService {
     private final FileDBRepository fileDBRepository;
     private final UserRepository userRepository;
+
+    private User getUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
+        return userRepository.findByEmail(currentPrincipalName).get();
+    }
+
 }
