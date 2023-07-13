f34283e2dfbe963ff39749546a82f2648059de12
Maksim Muhomedyarov <madmaxim22@gmail.com>
2023-07-13 22:11:52 +0300

    feat: implements upload files

--- src/main/java/com/example/cloudservice/controller/FileController.java
+++ src/main/java/com/example/cloudservice/controller/FileController.java
@@ -1,13 +1,28 @@
 package com.example.cloudservice.controller;
 
+import com.example.cloudservice.controller.autentication.ErrorResponse;
 import com.example.cloudservice.service.FileStorageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
 public class FileController {
     private final FileStorageService fileStorageService;
 
-
+    @PostMapping("/file")
+    public ResponseEntity<?> upload(@RequestParam String filename,
+                                    @RequestParam MultipartFile file) {
+        try {
+            fileStorageService.save(filename, file);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (IOException | IllegalArgumentException e) {
+            return new ResponseEntity<>(new ErrorResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
+        }
+    }
 }
--- src/main/java/com/example/cloudservice/service/FileStorageService.java
+++ src/main/java/com/example/cloudservice/service/FileStorageService.java
@@ -1,18 +1,36 @@
 package com.example.cloudservice.service;
 
 import com.example.cloudservice.model.authentication.User;
+import com.example.cloudservice.model.file.FileDB;
 import com.example.cloudservice.repository.FileDBRepository;
 import com.example.cloudservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
 public class FileStorageService {
     private final FileDBRepository fileDBRepository;
     private final UserRepository userRepository;
+
+    @Transactional
+    public FileDB save(String filename, MultipartFile file) throws IOException {
+        var fileDB = FileDB.builder()
+                .name(filename)
+                .type(file.getContentType())
+                .size(file.getSize())
+                .data(file.getBytes())
+                .user(getUser())
+                .build();
+        fileDBRepository.save(fileDB);
+        return fileDB;
+    }
 
     private User getUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
