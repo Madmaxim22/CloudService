021c57a6e6b805c26157ccd99cbcb807e33522ac
Maksim Muhomedyarov <madmaxim22@gmail.com>
2023-07-13 22:04:46 +0300

    feat: create FileRepository

---
+++ src/main/java/com/example/cloudservice/repository/FileDBRepository.java
@@ -0,0 +1,17 @@
+package com.example.cloudservice.repository;
+
+import com.example.cloudservice.model.file.FileDB;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface FileDBRepository extends JpaRepository<FileDB, Long> {
+    List<FileDB> findAllByUser_Id(Long userId);
+
+    Optional<FileDB> findFirstByNameAndUser_Id(String filename, long userId);
+
+    long deleteByNameAndUser_Id(String filename, long userId);
+}
