2e52e78810e4d2ce24fc9636ebd997e5f0e532e8
Maksim Muhomedyarov <madmaxim22@gmail.com>
2023-07-29 11:00:28 +0300

    feat: implements error handling, logging security layer

--- src/main/java/com/example/cloudservice/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/cloudservice/security/JwtAuthenticationFilter.java
@@ -1,11 +1,12 @@
 package com.example.cloudservice.security;
 
-import com.example.cloudservice.repository.TokenRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,36 +19,38 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-
+    @Value("${application.security.jwt.header}")
+    private String jwtHeader;
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
-    private final TokenRepository tokenRepository;
+
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
-        final String authHeader = request.getHeader("auth-token");
+        final String authHeader = request.getHeader(jwtHeader);
         final String jwt;
-        final String userEmail;
+        String userEmail;
+
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
+            log.debug(request.getRequestURI() + " - header authentication is empty or not starts with Bearer");
             return;
         }
+
         jwt = authHeader.substring(7);
         userEmail = jwtService.extractUsername(jwt);
+
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             // получение данных пользователя из базы данных
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
-            // проверка срока действия токена
-            var isTokenValid = tokenRepository.findByToken(jwt)
-                    .map(t -> !t.isExpired() && !t.isRevoker())
-                    .orElse(false);
             // валидация токена и пользователя
-            if (jwtService.isTokenValid(jwt, userDetails) && isTokenValid) {
+            if (jwtService.isTokenValid(jwt, userDetails)) {
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
                         userDetails,
                         null,
@@ -58,6 +61,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
+        log.debug(request.getRequestURI() + " - email is empty or Authentication is null");
         filterChain.doFilter(request, response);
     }
 }
